3e85f33d13042ed22ea9e5f046d42fc3f5cf4aff
agyatvyakti <yashitprof@gmail.com>
2022-04-28 08:33:11 +0200

    added doc string

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -8,26 +8,31 @@ import flask
 
 # Input injection
 def transcode_file(request, filename):
+    '''this transcode the file'''
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
 def foo(request, user):
+    '''this assert user as admin'''
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
 # Pickles
 class RunBinSh(object):
+    '''thuis is my class'''
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
+    ''' imports url lib'''
     exec("import urllib%s as urllib" % version)
 
 @app.route('/')
 def index():
+    ''' this defines index'''
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
