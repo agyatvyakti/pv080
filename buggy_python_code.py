27b143b463defe1157de2b254133cf7812e65446
agyatvyakti <yashitprof@gmail.com>
2022-04-28 08:38:48 +0200

    change order of imports and name foo

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,9 +1,8 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
 import subprocess
 import base64
-import subprocess
+import cPickle
 import flask
 
 # Input injection
@@ -14,7 +13,7 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def foo_fn(request, user):
     '''this assert user as admin'''
     assert user.is_admin, 'user does not have access'
     # secure code...
